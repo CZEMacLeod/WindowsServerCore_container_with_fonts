44476f3d9ac798ddab1b44bfa200409ba48d2847
Cynthia Z E MacLeod <cynthia.macleod@cloud3d.ltd>
2023-08-20 23:41:15 +0100

    Working version

---
+++ Build.ps1
@@ -0,0 +1,35 @@
+param (
+    [Parameter(Mandatory=$false)][string][Alias("Source-Image")]$src_img = "mcr.microsoft.com/dotnet/framework/sdk",
+    [Parameter(Mandatory=$false)][string][Alias("Source-Tag")]$src_tag = "4.8-windowsservercore-ltsc2022",
+    [Parameter(Mandatory=$false)][string][Alias("Destination-Image")]$dest_img = "dotnetframeworksdkfonts",
+    [Parameter(Mandatory=$false)][string][Alias("Destination-Tag")]$dest_tag = $src_tag,
+    [Parameter(Mandatory=$false)][switch][Alias("Update-WinSxS")]$update_winsxs
+)
+
+$winsxs = "${pwd}\WindowsSource\"
+If (!(test-path $winsxs))
+{
+    mkdir $winsxs
+    $update_winsxs = $true
+}
+
+If ($update_winsxs) {
+    Invoke-Expression "docker run --rm -v $($winsxs):C:\WindowsSource\ mcr.microsoft.com/windows/server:ltsc2022 Robocopy C:\Windows\WinSXS\ C:\WindowsSource\ /MIR"
+}
+
+if ($dest_tag -eq $null) {
+    $dest_tag = $src_tag
+}
+
+if (Test-Path ".\tmp_container.txt") {
+    Remove-Item ".\tmp_container.txt"
+}
+Invoke-Expression "docker run --cidfile tmp_container.txt -v $($winsxs):C:\WindowsSource\:ro -v ${pwd}\Install:C:\Install:ro --entrypoint C:\Install\InstallFonts.cmd $($src_img):$($src_tag) c:\windows\system32\cmd.exe"
+$tmp_container = Get-Content .\tmp_container.txt -Raw
+#Invoke-Expression "docker wait `"$($tmp_container)`""
+Invoke-Expression "docker commit --change `"ENTRYPOINT null`" `"$($tmp_container)`" tmp_container:temp" #`"$($dest_img)`:$($dest_tag)`""
+Invoke-Expression "docker rm `"$($tmp_container)`""
+Remove-Item ".\tmp_container.txt"
+
+Invoke-Expression "docker build -t `"$($dest_img)`:$($dest_tag)`" CleanImage"
+Expression "docker rm tmp_container:temp"
---
+++ Install/InstallFonts.cmd
@@ -0,0 +1,5 @@
+dism /online /enable-feature /featurename:ServerCoreFonts-NonCritical-Fonts-MinConsoleFonts /Source:C:\WindowsSource\ /LimitAccess
+dism /online /enable-feature /featurename:ServerCoreFonts-NonCritical-Fonts-Support /Source:C:\WindowsSource\ /LimitAccess
+dism /online /enable-feature /featurename:ServerCoreFonts-NonCritical-Fonts-BitmapFonts /Source:C:\WindowsSource\ /LimitAccess
+dism /online /enable-feature /featurename:ServerCoreFonts-NonCritical-Fonts-TrueType /Source:C:\WindowsSource\ /LimitAccess
+dism /online /enable-feature /featurename:ServerCoreFonts-NonCritical-Fonts-UAPFonts /Source:C:\WindowsSource\ /LimitAccess
